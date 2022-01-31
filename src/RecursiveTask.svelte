124194ecfced0ba89347b55e3d553d61e673ed52
Elton Lin <eltonlin1998@gmail.com>
2022-01-31 16:35:16 -0500

    MVP: I can use it to hierarchically list all my tasks

--- src/RecursiveTask.svelte
+++ src/RecursiveTask.svelte
@@ -1,21 +1,32 @@
 <!-- Recursively display a task and all its subtasks -->
-<div style="margin-left: 20px">
-  <div class="keep-on-same-line">{taskObject.name}</div>
-  {#if taskObject.children.length > 0}
+<div 
+  style="margin-left: 20px; margin-bottom: 10px"
+  on:pointerenter={showSubtasks}
+  on:pointerleave={hideSubtasks}
+>
+  <div style="display: flex; align-items: center">
+    <div class="keep-on-same-line" style="font-size: 1.65rem">
+      {taskObject.name}
+    </div>
+    {#if isShowingSubtasks}
+      {#if !isTypingNewTask}
+        <div on:click={() => isTypingNewTask = true} style="margin-left: 14px; display: flex; align-content: center">
+          <div class="plus alt"></div>
+          <!-- <div style="margin-top: 1px">new sub-task</div> -->
+        </div>
+      {:else}
+        <input bind:value={newTask} on:keypress={detectEnterKey} style="width: 100%; margin-left: 5px"/>
+      {/if}
+    {/if}
+  </div>
+
+  <div style="margin-top: 10px;">
     {#each taskObject.children as child, i}
-      <!-- Refer to blackboard video (URL coming soon) -->
       <RecursiveTask 
         taskObject={child}
         on:task-create={(e) => handleGrandchildUpdate(e, i)}
       />
     {/each}
-  {:else}
-    <!-- Base case -->
-  {/if}
-
-  <!-- New subtask -->
-  <div on:click={createNewChild}>
-    New subtask
   </div>
 </div>
 
@@ -26,6 +37,9 @@
   export let taskObject
 
   const dispatch = createEventDispatcher()
+  let isShowingSubtasks = false
+  let isTypingNewTask = false 
+  let newTask = ''
 
   /**
     * Creates a children payload that correctly reflects changes in grandchildren
@@ -46,12 +60,17 @@
     sendUpNewChildrenPayload(childrenCopy)
   }
 
-  function createNewChild() {
+  function detectEnterKey (e) {
+    if (e.charCode === 13) {
+      createNewChild(newTask)
+      newTask = ''
+      isTypingNewTask = false
+    }
+  }
+
+  function createNewChild (name) {
     sendUpNewChildrenPayload(
-      [
-        ...taskObject.children, 
-        { name: 'Fulfill MIT covid-19 requirements', id: 4, children: [] }
-      ]
+      [...taskObject.children, { name, children: [] }]
     )
   }
 
@@ -60,10 +79,41 @@
       updatedChildren: payload
     })
   }
+
+  function showSubtasks () {
+    isShowingSubtasks = true
+  }
+
+  function hideSubtasks () {
+    isShowingSubtasks = false
+  }
 </script>
 
 <style>
   .keep-on-same-line {
     white-space: nowrap;
+  }
+
+  .plus {
+    display:inline-block;
+    width:30px;
+    height:30px;
+    
+    background:
+      linear-gradient(#fff 0 0),
+      linear-gradient(#fff 0 0),
+      #000;
+    background-position:center;
+    background-size: 50% 2px,2px 50%; /*thickness = 2px, length = 50% (25px)*/
+    background-repeat:no-repeat;
+  }
+
+  .alt {
+    background:
+      linear-gradient(#000 0 0),
+      linear-gradient(#000 0 0);
+    background-position:center;
+    background-size: 50% 2px,2px 50%; /*thickness = 2px, length = 50% (25px)*/
+    background-repeat:no-repeat;
   }
 </style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,21 +1,25 @@
 <!-- TODO: 
-  1. Arrays and objects are not reactive by default, so ensure the UI updates properly as you change the task data structure
-  2. UI/CSS challenges 
-  3. Drag and drop
-  4. All the rest: integrating it with Supabase, Vercel, and deploying it
+  1. Drag and drop to calendar with height intervals
+  2. Event loops, habits (with counters), repeats and follow-ups 
+  3. Deadlines 
 -->
-
 <h1>Project Y</h1>
 <div style="display: flex;">
   <div>
     {#each allTasks as task}
-      <RecursiveTask 
-        on:task-create={(e) => modifyTaskTree(e, task)} 
-        taskObject={task}
-      >
+      <div style="width: 500px; border: solid; margin-bottom: 25px; padding-left: 0; padding-top: 25px; padding-bottom: 25px; padding-right: 12px;">
+        <RecursiveTask 
+          on:task-create={(e) => modifyTaskTree(e, task)} 
+          taskObject={task}
+        >
 
-      </RecursiveTask>
+        </RecursiveTask>
+      </div>
     {/each}
+    <div on:click={createTask} style="display: flex; align-content: center; justify-items: center">
+      <div class="plus alt" style="margin-left: 12px"></div>
+      <div style="margin-top: 2px; font-size: 1.65rem;">Create task</div>
+    </div>
   </div>
 
   <!-- Calendar section of today -->
@@ -29,27 +33,25 @@
       </div>
     {/each}
   </div>
-
-  <div on:click={createTask}>Create task</div>
 </div>
 
 <script>
   import RecursiveTask from '../RecursiveTask.svelte'
   import { onMount } from 'svelte'
-  // for example, if you have buy airpods, contact Amazon, etc.
+  import db from '../db.js'
+  import { collection, doc, getDoc, setDoc, updateDoc, onSnapshot } from 'firebase/firestore'
+
+  let allTasks = []
+
+  async function fetchTasks () { 
+    const user = await getDoc(
+      doc(db, 'users/GxBbopqXHW0qgjKEwU4z')
+    )
+    allTasks = user.data().allTasks
+  }
+  fetchTasks()
 
   $: scheduledTasks = allTasks.filter(task => task.time !== undefined)
-
-  let allTasks = [{
-    name: 'Visit UK friends & family',
-    id: 1,
-    time: 12,
-    children: [{
-      name: 'Visit ASR friends',
-      id: 2,
-      children: []
-    }]
-  }]
 
   onMount(() => {
     console.log('mounted(), scheduledTasks =', scheduledTasks)
@@ -78,18 +80,52 @@
       id: 3,
       children: []
     }]
-    console.log('allTasks is now =', allTasks)
   }
 
   function modifyTaskTree (e, task) {
+    // change the reference
     const { updatedChildren } = e.detail
     task.children = [...updatedChildren]
+
+    updateDoc(
+      doc(db, 'users/GxBbopqXHW0qgjKEwU4z'),
+      { allTasks }
+    )
+
     // change the pointer to the updatedChildren
     // allTasks' subtree is mutated, and it will correctly reference it,
-    // but the reactivity system does not KNOW it has been mutated, 
+    
+    // but the reactivity system does not KNOW when exactly it has been mutated, 
+
+    // source of truth should be the database
     allTasks = [...allTasks]
   }
 
   let timesOfTheDay = [8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
 </script>
+
+<style>
+  .plus {
+    display:inline-block;
+    width:35px;
+    height:35px;
+    
+    background:
+      linear-gradient(#fff 0 0),
+      linear-gradient(#fff 0 0),
+      #000;
+    background-position:center;
+    background-size: 50% 2px,2px 50%; /*thickness = 2px, length = 50% (25px)*/
+    background-repeat:no-repeat;
+  }
+
+  .alt {
+    background:
+      linear-gradient(#000 0 0),
+      linear-gradient(#000 0 0);
+    background-position:center;
+    background-size: 50% 2px,2px 50%; /*thickness = 2px, length = 50% (25px)*/
+    background-repeat:no-repeat;
+  }
+</style>
 
