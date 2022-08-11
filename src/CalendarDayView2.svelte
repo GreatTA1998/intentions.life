da6d3a75c2ace6ff4fa846e9d3cda3d629885ebb
Elton Lin <eltonlin1998@gmail.com>
2022-08-11 11:45:03 +0800

    Calendar day view will show sensible hour ranges, instead of always starting from midnight

--- src/CalendarDayView2.svelte
+++ src/CalendarDayView2.svelte
@@ -7,7 +7,7 @@
 <div style="overflow-y: scroll; overflow-x: hidden; height: 77vh">
   <div 
     id="calendar-day-container" 
-    style="position: relative; width: 12vw; height: 1500px"
+    style="position: relative; width: 12vw; height: 1000px"
     on:drop={(e) => drop_handler(e)}
     on:dragover={dragover_handler}
   >
@@ -18,7 +18,7 @@
         class="time-indicator" 
         style="top: {30 + (60 * i)}px;"
       >
-        {timeOfDay}
+        {timeOfDay.substring(0, 5)}
         
       </div>
     {/each}
@@ -68,28 +68,57 @@
 
   const dispatch = createEventDispatcher()
   const getDate = getDateOfToday
-  const timesOfDay = ['01:00', '02:00', '03:00', '04:00', '05:00', '06:00', '07:00', '08:00', '09:00', '10:00', '11:00', '12:00', '13:00', '14:00', '15:00', '16:00', '17:00', '18:00', '19:00', '20:00', '21:00', '22:00', '23:00', '00:00']
+  let timesOfDay = []
   let pixelsPerMinute = 90 / 60
   let startY = 0
 
-  let tasks = [
-    {
-      name: 'Launch Life Organizer',
-      startTime: '08:00',
-      duration: 120
-    },
-    {
-      name: 'Meditate',
-      startTime: '16:00',
-      duration: 10
+  let calendarStartTime = ''
+
+  // Start either from the current hour, or the current ongoing task's start time
+  //   - if it's 8 pm, but there's an ongoing task that started at 7 pm and hasn't ended
+  //     we start the calendar at 7 pm to not suddenly lose sight of the task
+  function decideCalendarStartTime () {
+    // get current time
+    const today = new Date()
+    // note we don't care about the minutes, we always start from the exact hour
+    const currentMinutesFromMidnight = today.getHours() * 60 // `getHour()` between 0-23
+    // for all tasks, check if it "includes" current time
+    for (const task of scheduledTasks) { // (only includes today's)
+    const hhOfStart = task.startTime.substring(0, 2)
+      const taskStartMinutesFromMidnight = parseInt(hhOfStart) * 60
+      const taskEndMinutesFromMidnight = taskStartMinutesFromMidnight + task.duration 
+      if (taskStartMinutesFromMidnight < currentMinutesFromMidnight && currentMinutesFromMidnight < taskEndMinutesFromMidnight) {
+        calendarStartTime = `${hhOfStart}:00`
+      } 
     }
-  ]
+
+    if (!calendarStartTime) {
+      calendarStartTime = `${today.toLocaleTimeString().substring(0, 2)}:00`
+    }
+
+    let currentHour = today.getHours() // get the integer i.e. 0 to 23
+    // now generate 16 hours of time (so it covers, for example, 8 am - midnight)
+    for (let i = 0; i < 16; i++) {
+      if (currentHour === 24) {
+        currentHour = 0
+      }
+
+      if (currentHour < 10) {
+        timesOfDay.push('0' + currentHour + ':00')
+      } else {
+        timesOfDay.push(currentHour + ':00')
+      }
+
+      currentHour += 1
+    }
+  } 
+  decideCalendarStartTime()
 
   function computeOffset ({ startTime }) {
-    // compute how many hours ahead of 8 am `startTime` is
+    // compute how many hours ahead of calendar's starting hour
     const hh = startTime.slice(0, 2)
     const mm = startTime.slice(3, 5)
-    const hoursOffset = Number(hh) + (Number(mm) / 60) - 8 // 8 refers to "8 am"
+    const hoursOffset = Number(hh) + (Number(mm) / 60) - parseInt(calendarStartTime.substring(0, 2)) // 8 refers to "8 am"
     const pixelsPerHour = 90
     return hoursOffset * pixelsPerHour
   }
@@ -109,9 +138,15 @@
     e.dataTransfer.dropEffect = "move";
   }
 
+  /**
+   * 
+   * Calculate what the new time is by measuring offset from top of container
+   * 
+   * We don't just read the hh e.g. "24" from the hour block, 
+   * because the drop_handler is defined outside of those hour <divs>
+   */
   function drop_handler (e) {
     console.log('drop detected')
-
 
     e.preventDefault()
     const element = document.getElementById("calendar-day-container")
@@ -122,26 +157,30 @@
     const heightOfHour = 90 // defined in CSS below
     const calendarTopMargin = 20
 
+    // `n` represents time in PURE hours e.g. 8.24, 13.90
     // `e.layerY` gives the Y-coordinate with respect to the current container
     // otherwise the multiple scrolling contexts messes it up
-    let hoursOffset = (e.layerY) / heightOfHour
-
-    let n = hoursOffset 
+    let n = (e.layerY) / heightOfHour
     const decimal = n - Math.floor(n)
     const integer = Math.trunc(n)
     
+    // calculate minutes i.e. `mm` 
     let minutesOffset = decimal * 60
-    let hh = 8 + integer
+    let mm 
+    if (minutesOffset < 10) { //minutesOffset = minutesOffset.toPrecision(1)
+      mm = `0${minutesOffset.toPrecision(1)}`
+    } else {
+      mm = minutesOffset.toPrecision(2)
+    }
+
+    // calculate hours i.e. `hh`
+    const origin = parseInt(calendarStartTime.substring(0, 2))
+    let hh = origin + integer
     if (hh < 10) {
       hh = `0${hh}`
     }
-    if (minutesOffset < 10) {
-      minutesOffset = minutesOffset.toPrecision(1)
-      minutesOffset = `0${minutesOffset}`
-    } else {
-      minutesOffset = minutesOffset.toPrecision(2)
-    }
-    const scheduledTime = `${hh}:${minutesOffset}` 
+
+    const scheduledTime = `${hh}:${mm}` 
     console.log('scheduledTime =', scheduledTime)
 
     dispatch('task-scheduled', {
