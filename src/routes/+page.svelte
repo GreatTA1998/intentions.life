137b276a7acd8ce4d34c4190dd4d5c1fa7dcc43d
Elton Lin <eltonlin1998@gmail.com>
2024-01-10 11:24:04 +0800

    Make the home page responsive to different devices, so users can at least see the video properly

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,17 +10,17 @@
       
       <div slot="content" style="display: flex; flex-grow: 1; height: 100%; padding: 4%;">
         <div style="background: transparent; width: 90vw; min-width: 200px; height: 80vh; border-radius: 10px;">
-          <div style="color: var(--base-color); font-size: 4rem; font-weight: 600;">
-            Tree-like to-do list + calendar
+          <div class="hero-title">
+            Combine your todo-list with your calendar
           </div>
     
-          <div style="display: flex; margin-top: 24px;">
-            <div style="width: 700px; font-size: 18px; font-weight: 500; display: inline; color: grey;">
+          <div style="display: flex; margin-top: 24px; flex-wrap: wrap;">
+            <div class="secondary-description">
               Create lots of sub-tasks on your to-do list, and drag-and-drop them to the calendar freely.
               Price is $5/month, <div class="highlighted-words" style="color: var(--base-color)">no need for payment info</div>, so that you don't have to worry about cancelling if you don't end up using it.
             </div>
             
-            <div style="margin-left: auto; margin-right: 0px; display: flex; align-items: center;">
+            <div style="display: flex; align-items: center; flex-wrap: wrap; gap: 12px 0px;" class="action-buttons">
               <div
                 on:click={createTemporaryDemoAccount}
                 style="cursor: pointer; border-radius: 24px; background-color: rgb(160, 160, 160); display: flex; align-items: center; justify-content: center; height: 40px; color: white; padding: 6px 24px; margin-right: 12px;">
@@ -38,7 +38,7 @@
 
     
           <div style="display: flex; justify-content: center; margin-top: 48px;">
-            <div style="height: 80vh; width: 90vw">
+            <div class="demo-video-container">
               <div style="position: relative; padding-bottom: 56.25%; height: 0;"><iframe src="https://www.loom.com/embed/5d5b7edd67534940a26268d0331d9671?sid=293776fa-384c-4e96-946a-2db2caabb412" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen style="position: absolute; top: 0; left: 0; width: 100%; height: 100%;"></iframe></div>
             </div>
           </div>
@@ -91,7 +91,7 @@
   }
 </script>
 
-<style>
+<style lang="scss">
   .transparent-glow-navbar {
     background-color: rgba(150, 150, 150, 0.1);
     border-bottom: none;
@@ -128,4 +128,70 @@ a:focus {
   background-color: #fff;
   color: #333;
 }
+
+.secondary-description {
+  font-weight: 500; display: inline; color: rgb(100, 100, 100);
+}
+
+.hero-title {
+  color: var(--base-color); 
+  font-size: 4rem; 
+  font-weight: 600;
+}
+
+.demo-video-container {
+  height: 80vh; width: 90vw
+}
+
+// note there will be a 1px boundary condition that causes an error, however, 
+// Kevin Powell's inequality syntax doesn't work for now https://youtube.com/shorts/mrzA2B5gUmI?si=J4yAzq1EQO-BwlN9
+
+
+// less than or equal to 768px
+@media (max-width: 1279.99px) {
+  .secondary-description {
+    width: 90vw;
+  }
+
+  .hero-title {
+    font-size: 7vw;
+  }
+
+  .action-buttons {
+    margin-top: 24px;
+  }
+}
+
+@media (max-width: 767.99px) {
+  .hero-title {
+    margin-top: 12px;
+    font-size: 10vw;
+  }
+
+  .demo-video-container {
+    width: 100vw; 
+    height: calc(100vw * 3/4);
+    margin-left: -4%; 
+    margin-right: -4%;
+  }
+}
+
+
+// greater than or equal to 768px;
+@media (min-width: 1280px) {
+  .hero-title {
+    margin-left: -6px;
+    font-size: 80px;
+  }
+
+  .secondary-description {
+    width: 54vw; 
+    font-size: 20px;
+  }
+
+  .action-buttons {
+    width: 30vw; 
+  }
+}
+
 </style>
