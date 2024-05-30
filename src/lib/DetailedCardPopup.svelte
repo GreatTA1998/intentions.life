7de521b14f403ae9ad6a4dc544029a6820b49ee6
Elton Lin <eltonlin1998@gmail.com>
2024-05-30 16:43:46 +0900

    Can merge photo with task

--- src/lib/DetailedCardPopup.svelte
+++ src/lib/DetailedCardPopup.svelte
@@ -43,11 +43,9 @@
 
       <div style="margin-top: 0px; display: flex; align-items: center; justify-content: space-between; flex-wrap: wrap; gap: 12px;">
         <div style="display: flex; align-items: center; width: 100%;">
-          <!-- {#if taskObject.imageDownloadURL}
-            <span class="material-symbols-outlined"  on:click={() => isViewingPhoto = !isViewingPhoto} style="cursor: pointer; margin-left: 6px; border: 1px solid grey; border-radius: 24px; padding: 4px;">
-              image_search
-            </span>
-          {/if} -->
+          {#if !taskObject.imageDownloadURL}
+            <MobileDetailedCardPopupPhotoUpload {taskObject}/>
+          {/if}
 
           <span class="material-symbols-outlined" on:click|stopPropagation={confirmDelete} 
             style="cursor: pointer; margin-left: auto; right: 0px; border: 1px solid grey; border-radius: 24px; padding: 4px;"
@@ -57,7 +55,7 @@
         </div>
       </div>
 
-        <div style="font-size: 1rem; margin-top: 0px; margin-bottom: 12px; font-weight: 400;">
+        <div style="font-size: 1rem; margin-top: 16px; margin-bottom: 12px; font-weight: 400;">
           Full ancestral tree
         </div>
 
@@ -94,6 +92,7 @@ import UXFormTextArea from '$lib/UXFormTextArea.svelte'
 import ReusableRoundButton from '$lib/ReusableRoundButton.svelte'
 import ReusableCheckbox from '$lib/ReusableCheckbox.svelte'
 import DetailedCardPopupStartTimeDuration from '$lib/DetailedCardPopupStartTimeDuration.svelte'
+import MobileDetailedCardPopupPhotoUpload from '$lib/MobileDetailedCardPopupPhotoUpload.svelte'
 
 export let taskObject 
 
---
+++ src/lib/MobileDetailedCardPopupPhotoUpload.svelte
@@ -0,0 +1,86 @@
+<span class="material-symbols-outlined"  
+  on:click={() => FolderInput.click()} 
+  style="cursor: pointer; margin-left: 6px; border: 1px solid grey; border-radius: 24px; padding: 4px;"
+>
+  add_photo_alternate
+</span>
+
+<input style="display: none;" 
+  bind:this={FolderInput}
+  on:change={(e) =>  handleFileChange(e)} 
+  type="file" 
+  accept="image/*" 
+>
+
+<script>
+  import { getStorage, ref, uploadBytes, getDownloadURL } from "firebase/storage"
+  import { updateFirestoreDoc } from '/src/crud.js'
+  import { getRandomID, checkTaskObjSchema, getDateInMMDD, getTimeInHHMM } from '/src/helpers.js'
+  import { user } from '/src/store.js'
+  import { goto } from '$app/navigation'
+  import { onMount } from 'svelte'
+
+  export let taskObject
+
+  let FolderInput
+  const storage = getStorage()
+
+  async function handleFileChange (e) {
+    const promises = []
+    for (const imageBlobFile of e.target.files) {
+      if (imageBlobFile) {
+        const id = getRandomID()
+        promises.push(
+          uploadImageBlobToFirebase(imageBlobFile, id).then(resultSnapshot => {
+            mergeImageWithTask(resultSnapshot, imageBlobFile, id)
+          })
+        )
+      }
+    }
+    await Promise.all(promises)
+    alert('Photos successfully uploaded.')
+  }
+
+  async function mergeImageWithTask (resultSnapshot, imageBlobFile, id) {
+    const { metadata } = resultSnapshot 
+    const { fullPath, timeCreated } = metadata
+
+    // STEP 0: parallel process to retrieve width & height
+    let durationForFullDisplay
+    const p1 = createImageBitmap(imageBlobFile).then(bitmap => {
+      const { width, height } = bitmap 
+      // these durations will display fully the portrait / landscape iPhone photos on an iPad Air 1180x820
+      if (width > height) durationForFullDisplay = 106
+      else durationForFullDisplay = 188
+    })
+
+    // STEP 1: getDownloadURL()
+    let imageDownloadURL 
+    const p2 = getDownloadURL(ref(storage, fullPath)).then(url => imageDownloadURL = url)
+    
+    await Promise.all([p1, p2])
+
+    // STEP 2: create a task scheduled at the same time the photo is taken
+    let dateClassObj 
+    if (imageBlobFile.lastModified) dateClassObj = new Date(imageBlobFile.lastModified)
+    else dateClassObj = new Date(timeCreated) // otherwise we set the time to right now.
+
+    updateFirestoreDoc(`/users/${$user.uid}/tasks/${taskObject.id}`, {
+      duration: durationForFullDisplay,
+      imageDownloadURL,
+      imageFullPath: fullPath, // for easy garbage collection
+      startTime: getTimeInHHMM({ dateClassObj }),
+      startDate: getDateInMMDD(dateClassObj), // MMDD is a legacy function so doesn't use destructuring
+      startYYYY: `${dateClassObj.getFullYear()}` // year needs to be a string for some reason
+    })
+  }
+
+  async function uploadImageBlobToFirebase (blobFile, id) {
+    return new Promise(async (resolve) => {
+      const storageRef = ref(storage, `images/${id}`)
+      const snapshot = await uploadBytes(storageRef, blobFile)
+      console.log('Uploaded a blob or file! =', snapshot);
+      resolve(snapshot)
+    })
+  }
+</script>
