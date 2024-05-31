9278e7a9e83e1b4728c6c14f9fc42e6a1fe3db7e
Elton Lin <eltonlin1998@gmail.com>
2024-05-31 16:10:36 +0900

    Generate preview

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -14,7 +14,7 @@
           <video 
             controls
             on:error={(error) => alert('error =', error)}
-            src="https://firebasestorage.googleapis.com/v0/b/project-y-2a061.appspot.com/o/homePageDemoVideos%2Ffeature-1.mp4?alt=media&token=4bc3e4c2-778a-4ece-ae6a-604cc60e98ce">
+            src="https://firebasestorage.googleapis.com/v0/b/project-y-2a061.appspot.com/o/homePageDemoVideos%2Ffeature-1.mp4?alt=media&token=4bc3e4c2-778a-4ece-ae6a-604cc60e98ce#t=0.1">
           </video>
           
           <div class="hero-title">
