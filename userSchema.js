387ba0fac8d00f8bd14236487c9ed6e0f819bfb6
Maryus <martsyalis@example.com>
2024-08-02 17:27:38 +0900

    add Schema and marius file

---
+++ userSchema.js
@@ -0,0 +1,30 @@
+user = {
+   name: '',
+   email: '',
+   tasks: [taskId]
+}
+
+
+tasks = {
+    taskID: taskTreeObj
+}
+
+
+
+Collection1
+user = {
+    name: '',
+    email: '',
+ }
+
+ Collection2
+ 
+tasks = {
+    userId: [taskTreeObj]
+}
+
+
+
+taskTreeObj= {task: 'name', date: '', parentId: 'parentId'}
+
+
