efafa81d52f046326eb270d7f850235bf655478c
Marcelis <marsalius1@gmail.com>
2024-08-27 11:13:03 +0900

    less jaring image

--- src/lib/MainPage.svelte
+++ src/lib/MainPage.svelte
@@ -1,8 +1,8 @@
 
 
 {#if isMobile}
-<img class='mobile-mode-coming-image' src='https://firebasestorage.googleapis.com/v0/b/project-y-2a061.appspot.com/o/websiteImages%2F57668dc2-ef2b-4f71-a54d-dc2e1746b219.webp?alt=media&token=29b2fd82-7a11-494c-91fe-ebdfadc58873'/>
 <h1 class='mobile-mode-coming-text'> Mobile mode is coming </h1>
+<img class='mobile-mode-coming-image' src='https://firebasestorage.googleapis.com/v0/b/project-y-2a061.appspot.com/o/websiteImages%2F310c3749-fea0-4044-b7ad-1a27fb15c371.webp?alt=media&token=77859183-83a5-4b83-9ee3-d77fff15a783'/>
 {:else}
 {#key clickedTask}
   {#if isDetailedCardOpen}
@@ -732,19 +732,20 @@ const checkMobile = () => {
 
     .todo-container {
       overflow: visible;
+      
     }
   }
   .mobile-mode-coming-image{
     max-width: 100vw;
     object-fit: cover;
     object-position: center;
-    margin-top: 20vh;
     padding-left: 10px;
     padding-right: 10px;
-    
   }
+
   .mobile-mode-coming-text{
     text-align: center;
-    margin: 50px auto;
+    margin: 10vh auto;
+
   }
 </style>
