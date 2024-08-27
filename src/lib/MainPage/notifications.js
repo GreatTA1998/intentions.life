996f2becb80de91eff6f7c7d8469b7db29d7c8ec
Marcelis <marsalius1@gmail.com>
2024-08-28 00:15:28 +0900

    getToken in notifications

--- src/lib/MainPage/notifications.js
+++ src/lib/MainPage/notifications.js
@@ -1,6 +1,4 @@
-import {
-  getMessaging,
-} from "firebase/messaging";
+import { getMessaging, getToken } from "firebase/messaging";
 import { app } from "../../back-end/firestoreConnection";
 export const handleNotificationPermission = () => {
   console.log("Requesting permission...");
@@ -14,22 +12,42 @@ export const handleNotificationPermission = () => {
 };
 
 export const handleSW = () => {
-    console.log("we are in handleSW");
-    const messaging = getMessaging(app);
-    if ("serviceWorker" in navigator) {
-      navigator.serviceWorker
-        .register("/firebase-messaging-sw.js")
+  console.log("we are in handleSW");
+  const messaging = getMessaging(app);
 
-        .then((registration) => {
-          console.log(
-            "Service Worker registered with scope:",
-            registration.scope
-          );
-        })
-        .catch((error) => {
-          console.error("Service Worker registration failed:", error);
-        });
-    } else {
-      console.error("Service Worker not supported in this browser.");
-    }
+  getToken(messaging, {
+    vapidKey: import.meta.env.VITE_PUBLIC_MESSAGING_VAPID_KEY,
+  })
+    .then((currentToken) => {
+      if (currentToken) {
+        // Send the token to your server and update the UI if necessary
+        // ...
+        console.log("current token is", currentToken);
+      } else {
+        // Show permission request UI
+        console.log(
+          "No registration token available. Request permission to generate one."
+        );
+        // ...
+      }
+    })
+    .catch((err) => {
+      console.log("An error occurred while retrieving token. ", err);
+    });
+  if ("serviceWorker" in navigator) {
+    navigator.serviceWorker
+      .register("/firebase-messaging-sw.js")
+
+      .then((registration) => {
+        console.log(
+          "Service Worker registered with scope:",
+          registration.scope
+        );
+      })
+      .catch((error) => {
+        console.error("Service Worker registration failed:", error);
+      });
+  } else {
+    console.error("Service Worker not supported in this browser.");
+  }
 };
--- static/firebase-messaging-sw.js
+++ static/firebase-messaging-sw.js
@@ -1,8 +1,12 @@
-importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-app-compat.js');
-importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-messaging-compat.js');
+importScripts(
+  "https://www.gstatic.com/firebasejs/9.0.0/firebase-app-compat.js"
+);
+importScripts(
+  "https://www.gstatic.com/firebasejs/9.0.0/firebase-messaging-compat.js"
+);
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyCOVm0X6UUQQcftXf066z_0hFk497j4dNY',
+  apiKey: "AIzaSyCOVm0X6UUQQcftXf066z_0hFk497j4dNY",
   authDomain: "project-y-2a061.firebaseapp.com",
   projectId: "project-y-2a061",
   storageBucket: "project-y-2a061.appspot.com",
@@ -11,19 +15,19 @@ const firebaseConfig = {
   measurementId: "G-EZSLE84PYQ",
 };
 
-
 firebase.initializeApp(firebaseConfig);
 
 const messaging = firebase.messaging();
 
+
 // Optional: Add background message handler
 messaging.onBackgroundMessage((payload) => {
-  console.log('Received background message: ', payload);
+  console.log("Received background message: ", payload);
   // Customize notification here
   const notificationTitle = payload.notification.title;
   const notificationOptions = {
     body: payload.notification.body,
-    icon: '/firebase-logo.png'
+    icon: "/firebase-logo.png",
   };
 
   self.registration.showNotification(notificationTitle, notificationOptions);
